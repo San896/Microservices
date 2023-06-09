afc5f9f5a8f407048cbdd2d4ad65c1932dc9f070
san896 <sannlfernandez8@gmail.com>
2023-06-09 00:20:39 -0300

    changes

---
+++ db/index.js
@@ -0,0 +1,6 @@
+
+const server = require("./src/server");
+
+server.listen(8004, () => {
+    console.log("Database service on Port 8004")
+});
---
+++ db/src/database/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { MONGO_URI } = require("../config/envs")
+
+const conn = mongoose.createConnection(MONGO_URI);
+
+const Character = conn.model("Character",  require("./schemas/characterSchema"));
+const Film = conn.model("Film",  require("./schemas/filmSchema"));
+const Planet = conn.model("Planet",  require("./schemas/planetSchema"));
+
+Character.find()
+.populate("films")
+.then(res => console.log(res))
+
+
+
+module.exports = {
+    Character: Character,
+    Film: Film,
+    Planet: Planet,
+}
